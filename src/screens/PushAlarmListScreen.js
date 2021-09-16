9c2a710a2e63da3f2e96dd8b9226a5f6a2bcf09d
Seo Jun Yoo <sytwoyou@gmail.com>
2021-09-17 00:44:29 +0900

    Feat: push notification permission

--- src/screens/PushAlarmListScreen.js
+++ src/screens/PushAlarmListScreen.js
@@ -1,12 +1,52 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
+import PushNotificationCard from "../components/PushNotificationCard";
 
 const PushAlarmListScreen = () => {
+  const newPushNotificationList = [];
+  const pastPushNotificationList = [];
+
   return (
-    <View>
-      <Text>push alarm</Text>
+    <View style={styles.container}>
+      <View style={styles.newNotification}>
+        <Text style={styles.dividerText}>
+          NEW
+        </Text>
+      </View>
+      <PushNotificationCard />
+
+      <View style={styles.pastNotification}>
+        <Text style={styles.dividerText}>
+          Past
+        </Text>
+      </View>
+      <PushNotificationCard />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  newNotification: {
+    alignItems: "center",
+    width: 150,
+    marginVertical: 30,
+    borderBottomWidth: 1,
+  },
+  pastNotification: {
+    alignItems: "center",
+    width: 150,
+    marginVertical: 30,
+    borderBottomWidth: 1,
+  },
+  dividerText: {
+    fontSize: 15,
+    marginBottom: 5,
+  },
+});
 
 export default PushAlarmListScreen;
