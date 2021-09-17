d772e946a9f2a441f36bb5623a77fe752b2245be
Seo Jun Yoo <sytwoyou@gmail.com>
2021-09-18 03:50:44 +0900

    Fix: modify environment variable setting

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,8 +1,10 @@
-import { IOS_CLIENT_ID, ANDROID_CLIENT_ID } from "@env";
+import getEnvVars from "../../environment";
+
+const { IOS_CLIENT_ID, ANDROID_CLIENT_ID } = getEnvVars();
 
 const CLIENT_ID = {
-  androidClientId: ANDROID_CLIENT_ID,
-  iosClientId: IOS_CLIENT_ID,
+  androidStandaloneAppClientId: ANDROID_CLIENT_ID,
+  iosStandaloneAppClientId: IOS_CLIENT_ID,
   scopes: ["profile", "email"],
 };
 
